c2608710dc10f47143ad89945339bbdcf6254338
3412ysw <3412ysw@gmail.com>
2025-01-15 19:31:31 +0900

    [ysw] 회원가입, 로그인 기능 완료

--- cinema_project/src/cinema_project/run/Run.java
+++ cinema_project/src/cinema_project/run/Run.java
@@ -1,7 +1,13 @@
 package cinema_project.run;
 
+import cinema_project.view.CinemaMenu;
+
 public class Run {
 	public static void main(String[] args) {
+		
+		CinemaMenu cm = new CinemaMenu();
+		cm.mainMenu();
+
 		
 	}
 }
--- cinema_project/src/cinema_project/view/CinemaMenu.java
+++ cinema_project/src/cinema_project/view/CinemaMenu.java
@@ -21,7 +21,7 @@ public class CinemaMenu {
 			int menu = sc.nextInt();
 			sc.nextLine();
 			switch(menu) {
-				case 1 : signIn(); break;
+				case 1 : insertUser(); break;
 				case 2 : login(); break;
 				case 0 : System.out.println("이용해주셔서 감사합니다."); return;
 				default : System.out.println("메뉴를 잘못 입력하셨습니다.");
@@ -29,29 +29,63 @@ public class CinemaMenu {
 		}
 	}
 	// 회원가입 
-	public void signIn() {
+	public void insertUser() {
 		System.out.println("*** 회원 가입 ***");
+		while(true) {
+			System.out.print("아이디 입력 : ");
+			String userId = sc.nextLine();
+			UserVo check = cc.checkUserId(userId);
+			if(check != null) {
+				System.out.println("중복되는 아이디입니다. 다시 입력해주세요.");
+			}else {
+				while(true) {
+					System.out.print("비밀번호 입력 : ");
+					String userPw = sc.nextLine();
+					System.out.print("비밀번호 확인 : ");
+					String checkPw = sc.nextLine();
+					if(!userPw.equals(checkPw)) {
+						System.out.println("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
+					}else {
+						System.out.print("이름 입력 : ");
+						String userName = sc.nextLine();
+						System.out.print("생년월일 (ex)2002-06-15 : ");
+						String userBirth = sc.nextLine();
+						System.out.print("이메일 : ");
+						String userEmail = sc.nextLine();
+						System.out.print("전화번호(-) : ");
+						String userPhone = sc.nextLine();
+						int result = cc.insertUser(userId, userPw, userName, userBirth, userEmail, userPhone);
+						printResult(result, "회원가입");
+						break;
+					}
+				}break;
+			}
+		}	
 	}
+
 	// 로그인
 	public void login() {
 		System.out.println("*** 로그인 ***");
 		System.out.print("아이디 : ");
-		String id = sc.nextLine();
+		String userId = sc.nextLine();
 		System.out.print("비밀번호 : ");
-		String pw = sc.nextLine();
-		
-//		UserVo user = cc.loginMember(id,pw);
-		UserVo user = new UserVo();
+		String userpw = sc.nextLine();
+		UserVo user = cc.login(userId, userpw);
 		if(user != null) {
-			// 관리자 아이디로 로그인시 관리자메뉴로 이동
-			if(id.equals("admin")) {
-				System.out.println("*** 관리자 메뉴 ***");
+			System.out.println(user.getUser_name()+"님 환영합니다!");
+			if(user.getUser_id().equals("admin")) {
+				//관리자 메뉴 호출 부탁해염 
 			}else {
-				//유저 메뉴
 				userMenu(user);
 			}
-		}else System.out.println("아이디 혹은 비밀번호가 잘못되었습니다.");
+		}else {
+			System.out.println("비밀번호 혹은 아이디가 일치하지 않습니다.");
+		}
+		
 	}
+
+	
+	
 	
 	// 사용자 메뉴
 	public void userMenu(UserVo user) {
@@ -131,4 +165,15 @@ public class CinemaMenu {
 			}else System.out.println("회원 탈퇴에 실패하였습니다.");
 		}else System.out.println("비밀번호를 다시 확인해주세요.");
 	}
+	
+	// 결과 확인하는 메소드 
+		public void printResult(int result, String menuName) {
+			if(result > 0) {
+				System.out.println(menuName+ "이(가) 정상적으로 완료되었습니다.");
+			}else {
+				System.out.println(menuName+" 중 오류가 발생되었습니다.");
+			}
+			
+		}
+	
 }
